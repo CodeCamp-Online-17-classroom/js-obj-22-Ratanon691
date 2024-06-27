68eb29d87aa12ecd60e7a17e88f51c19dab0a091
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-28 01:48:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+function Accumulator() {
+  this.startingValue = 0
+  this.currentValue = 0
+  this.input = 0
+  this.read = function(){
+    this.input = +prompt(`Give me a number`)
+     return this.currentValue += this.input
+  }
+  this.show = function(){
+    alert(this.currentValue)
+  }
+}
+  let num1 = new Accumulator()
+  num1.read()
+  num1.show()
